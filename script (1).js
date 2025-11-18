48104700576ea989e1289f49580d00ee92ecb9d1
PedrowwLuis <pedroluisdeliraalves@gmail.com>
2025-11-18 19:22:11 -0300

    :wrench: Correção dos Nomes dos Arquivos

--- script (1).js
+++
@@ -1,7 +0,0 @@
-// Script simples de confirmação de agendamento
-document.getElementById('form-agendamento').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('✅ Agendamento realizado com sucesso!');
-    this.reset();
-  });
-  
