474178347c6b9dbd6eac35e7c13fa77b0be63798
PedrowwLuis <pedroluisdeliraalves@gmail.com>
2025-11-26 08:06:11 -0300

    🔧 ⬆️ Alteração do BD

--- barbalab.sql
+++ barbalab.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3310
--- Tempo de geração: 19-Nov-2025 às 14:36
--- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.4.3
+-- Host: 127.0.0.1
+-- Tempo de geração: 26/11/2025 às 12:03
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `agendamento`
+-- Estrutura para tabela `agendamento`
 --
 
 CREATE TABLE `agendamento` (
@@ -33,53 +32,80 @@ CREATE TABLE `agendamento` (
   `nome` varchar(150) NOT NULL,
   `email` varchar(255) NOT NULL,
   `serviços` varchar(150) NOT NULL,
-  `data` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `data` date NOT NULL,
+  `hora` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `agendamento`
+--
+
+INSERT INTO `agendamento` (`id`, `nome`, `email`, `serviços`, `data`, `hora`) VALUES
+(1, 'luca', 'luca@gmail.com', 'Corte de Cabelo e Barba', '2025-11-25', '12:00:00'),
+(3, 'Pedro', 'lucas@gmail.com', 'Corte de Cabelo e Barba', '2025-11-25', '13:00:00'),
+(6, 'Pedro', 'lucas@gmail.com', 'Cabelo e Barba + Sobrancelha + Hidratação', '2025-12-06', '13:00:00'),
+(8, 'adriano', 'adiano@gmail.com', 'Cabelo + Sobrancelha + Pigmentação + Hidratação', '2025-12-12', '13:00:00');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cadastro_cliente`
+-- Estrutura para tabela `cadastro_cliente`
 --
 
 CREATE TABLE `cadastro_cliente` (
-  `id` int(11) NOT NULL,
   `nome` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
   `senha` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cadastro_cliente`
+--
+
+INSERT INTO `cadastro_cliente` (`nome`, `email`, `senha`) VALUES
+('Pedro', 'pedroluisdeliraalves@gmail.com', '123456'),
+('Pedro', 'lucas@gmail.com', '123456'),
+('luca', 'luca@gmail.com', 'luca'),
+('adriano', 'adiano@gmail.com', '123456');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `login_adm`
+-- Estrutura para tabela `login_adm`
 --
 
 CREATE TABLE `login_adm` (
   `nome` varchar(150) NOT NULL,
   `senha` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `login_adm`
+--
+
+INSERT INTO `login_adm` (`nome`, `senha`) VALUES
+('[admin]', '[admin]'),
+('admin', 'admin');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `agendamento`
+-- Índices de tabela `agendamento`
 --
 ALTER TABLE `agendamento`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`nome`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `agendamento`
 --
 ALTER TABLE `agendamento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
